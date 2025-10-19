7b1eb9c3ba2ded9c0b91e95f9f6622f4892b7ef4
adxtyax06 <realadityax06@gmail.com>
2025-10-19 15:36:03 +0530

    Quartz sync: Oct 19, 2025, 3:36 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -18,6 +18,7 @@ const config: QuartzConfig = {
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian", ".readme"],
+    homePage: "index.md"
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
